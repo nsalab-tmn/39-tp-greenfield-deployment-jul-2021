ebec89901b7634abd3202796c20be61e50235455
ruzmuh <ruzmuh@gmail.com>
2021-07-19 17:46:41 +0500

    rg instantiantion

--- de39-2021.tf
+++ de39-2021.tf
@@ -2,9 +2,10 @@ module "competion" {
     source = "./terraform"
     count = 1
     prefix = format("comp-%02d", count.index+1)
-    deploy_routes = true
-    deploy_dns_a_records = true
-    deploy_custom_data = true
+    deploy_routes = false
+    deploy_dns_a_records = false
+    deploy_custom_data = false
+    competition_instance = "gfd-39"
 }
 
 output "passwords" {
